12112e028d48c4a4024ca575ae6b1d57333ea75c
Jerliyah <jerliyahcraig@gmail.com>
2018-01-11 16:21:24 -0600

    pause and play with click or spacebar

--- action.js
+++ action.js
@@ -6,3 +6,23 @@ TODO:
     - background (given list and upload)
     - video upload
 */
+
+const player = document.querySelector('div#player');
+const video = player.querySelector('video')
+
+
+// ----------- Helper Functions -----------------
+function toggle_play() {
+    video[ video.paused? 'play' : 'pause' ]()
+}
+
+// ------------ User Controls ---------------
+video.addEventListener('click', toggle_play);
+window.addEventListener("keypress", function (e) {
+    if (e.defaultPrevented) { return; /* Do nothing if the event was already processed */ }
+    if( e.key === " ") { toggle_play() }
+})
+
+
+
+// ------------ Customization options -------------
