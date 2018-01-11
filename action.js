f18d5e3ebdbbe946098a9f44575aa796b84a296d
Jerliyah <jerliyahcraig@gmail.com>
2018-01-11 16:48:54 -0600

    using google icons

--- action.js
+++ action.js
@@ -7,21 +7,28 @@ TODO:
     - video upload
 */
 
+// ------------- DOM grab ---------------
 const player = document.querySelector('div#player');
-const video = player.querySelector('video')
+const video = player.querySelector('video');
+const play_btn = player.querySelector('i#play');
 
 
 // ----------- Helper Functions -----------------
 function toggle_play() {
-    video[ video.paused? 'play' : 'pause' ]()
+    video[ video.paused? 'play' : 'pause' ]();
+    play_btn.innerText = video.paused? 'play_arrow' : 'pause'
 }
 
 // ------------ User Controls ---------------
+    // Pause and Play
+play_btn.addEventListener('click', toggle_play);
 video.addEventListener('click', toggle_play);
 window.addEventListener("keypress", function (e) {
-    if (e.defaultPrevented) { return; /* Do nothing if the event was already processed */ }
+    if (e.defaultPrevented) { return; /* Do nothing if the event was already processed */ } 
     if( e.key === " ") { toggle_play() }
 })
+
+
 
 
 
