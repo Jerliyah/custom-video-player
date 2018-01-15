91d538f967d8649b4960d5481825693180bc61df
Jerliyah <jerliyahcraig@gmail.com>
2018-01-15 17:47:20 -0600

    progress bar

--- action.js
+++ action.js
@@ -10,9 +10,10 @@ TODO:
 
 // ------------- DOM Stuff ---------------
 // Player and controls
-const player = document.querySelector('div#player');
+const player = document.querySelector('#player');
 const video = player.querySelector('video');
-const video_controls = player.querySelector('div#video-controls');
+const video_controls = player.querySelector('#video-controls');
+const progress_bar = player.querySelector('div#progress-bar');
 
 // Play
 const play_btn = player.querySelector('i#play');
@@ -36,7 +37,8 @@ var speeds = [0.5, 1, 1.5, 2]
 speeds.forEach( (speed) => {
     speed_ctn.insertAdjacentHTML('beforeend', `<li data-speed="${speed}">${speed}x</li>`)
 })
-const speed_options = speeds_ctn.querySelector('li');
+const speed_options = speed_ctn.querySelector('li');
+
 
 
 // ----------- Helper Functions -----------------
@@ -46,20 +48,26 @@ function toggle_play() {
 }
 
 function skip() {
-    let data = this.querySelector('i').dataset
+    let data = this.querySelector('i').dataset;
     let time_change = data.direction === 'forward'?  data.skip : "-" + data.skip;
-    console.log(video.currentTime)
-    video.currentTime += parseInt(time_change)
-    console.log(video.currentTime)
+    video.currentTime += parseInt(time_change);
 }
 
 function change_video_speed() {
     let speed = this.dataset.speed;
-    
+}
+
+function handle_progress() {
+    let progress = (video.currentTime / video.duration) * 100;
+    progress_bar.style.width = progress + "%";
 }
 
 
+
 // ------------ User Controls ---------------
+    // See Progess
+video.addEventListener('timeupdate', handle_progress);
+
     // Pause and Play
 play_btn.addEventListener('click', toggle_play);
 video.addEventListener('click', toggle_play);
@@ -74,6 +82,10 @@ skips.forEach( (skip_btn) => { skip_btn.addEventListener('click', skip) })
     // Speed Change
 speed_ctn.addEventListener('click', function() { this.classList.toggle('showing-options') })
 speed_options.addEventListener('click', change_video_speed)
+
+progress_bar.addEventListener('mouseover', handle_progress)
+
+
 
 
 
